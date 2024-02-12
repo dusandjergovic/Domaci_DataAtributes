50453c49c042d248f3b7362fc67e38b17d0b7fcf
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-02-12 20:31:22 +0100

    Ulepsavanje koda

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ const grid = document.querySelector('.grid');
 for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
     const cell = document.createElement('div');
-    // taj deo sam napisao
-    cell.addEventListener('click', function() {
-        cell.style.backgroundColor = 'black';
-    } )
-    //
-    cell.classList.add('cell');
-    // taj deo sam napisao    
+    cell.classList.add('cell');  
+    
     if((j % 2) === 0)  {
             cell.classList.add('crvena')
         } else {
             cell.classList.add('plava')
         }
-    //
+
+        cell.addEventListener('click', function() {
+            cell.style.backgroundColor = 'black';
+        } )
+
     grid.appendChild(cell);
   }
 }
