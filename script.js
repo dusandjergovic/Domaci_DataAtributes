6edd790a2dce92eabe1d3d1bd47e645042cc2477
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-02-12 20:13:51 +0100

    Domaci za DataAtributes

---
+++ script.js
@@ -0,0 +1,18 @@
+const grid = document.querySelector('.grid');
+
+
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    const cell = document.createElement('div');
+    cell.addEventListener('click', function() {
+        cell.style.backgroundColor = 'black';
+    } )
+    cell.classList.add('cell');
+        if((j % 2) === 0)  {
+            cell.classList.add('crvena')
+        } else {
+            cell.classList.add('plava')
+        }
+    grid.appendChild(cell);
+  }
+}
