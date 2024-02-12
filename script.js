06b2df6e192bb7e01e43fede55b7ab6c687d6d54
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-02-12 20:17:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const grid = document.querySelector('.grid');
 for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
     const cell = document.createElement('div');
+    // taj deo sam napisao
     cell.addEventListener('click', function() {
         cell.style.backgroundColor = 'black';
     } )
+    //
     cell.classList.add('cell');
-        if((j % 2) === 0)  {
+    // taj deo sam napisao    
+    if((j % 2) === 0)  {
             cell.classList.add('crvena')
         } else {
             cell.classList.add('plava')
         }
+    //
     grid.appendChild(cell);
   }
 }
